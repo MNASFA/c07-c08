5ded68db6e0e286a1731cd0dc0e122cb32640abc
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2024-07-15 20:35:46 +0100

    Add files via upload

---
+++ test/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmnasfa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/13 15:46:01 by hmnasfa           #+#    #+#             */
+/*   Updated: 2024/07/14 17:07:11 by hmnasfa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define EVEN(nbr) (nbr % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
---
+++ test/ex04/ft_strs_to_tab.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmnasfa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 17:49:03 by hmnasfa           #+#    #+#             */
+/*   Updated: 2024/07/15 09:50:18 by hmnasfa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+
+	dest = (char *)malloc(sizeof(char) * ft_strlen(str));
+	if (!str)
+		return (NULL);
+	ft_strcpy(dest, src);
+	return (dest);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*stock;
+
+	i = 0;
+	stock = (t_stock_str *)malloc((size + 1) * sizeof (t_stock_str));
+	if (!stock)
+		return (NULL);
+	while (i < ac)
+	{
+		stock[i].size = ft_strlen(av[i]);
+		stock[i].str = av[i];
+		stock[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	stock[i].size = 0;
+	stock[i].str = NULL;
+	stock[i].copy = NULL;
+	return (stock);
+}
